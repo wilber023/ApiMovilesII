b216daa65411d230d9f849d10a6ef05dc628fc71
wilber023 <whzvqz013@gmail.com>
2025-08-04 01:36:48 -0600

    notificaion

--- config/auth.js
+++ config/auth.js
@@ -1,17 +1,9 @@
-const jwt = require('jsonwebtoken');
+
 
 
 const JWT_SECRET = process.env.JWT_SECRET || 'mi_clave_secreta_super_segura';
-const JWT_EXPIRE = process.env.JWT_EXPIRE || '30d';
 
-
-const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, JWT_SECRET, {
-    expiresIn: JWT_EXPIRE
-  });
-};
 
 module.exports = {
-  JWT_SECRET,
-  generateToken
+  JWT_SECRET
 };
